d32ae5079ccfbd51a870fffb6658557c9e869575
Sawai12 <109336735+Sawai12@users.noreply.github.com>
2024-01-18 11:35:00 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token="5717007875:AAFuDj6aaKcIT31AZmUQRg2414CcZrjdP-g",
-  api_id=1654363,
-  api_hash="26b911420edb5ceb8f370f21f5eb2684"
+  bot_token="6619826204:AAHwE7rvSOLisD-aICSs-uMh3CobfYgXjIo",
+  api_id=27412454,
+  api_hash="9a657988cb949b70a313e0c84f14207e"
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
